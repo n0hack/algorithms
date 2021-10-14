dc7e1e85ca012b7397da3a28693ad8d55ffc3e67
Ji-Hun Jeon <nohack-@naver.com>
2021-10-14 13:11:07 +0900

    스택을 이용한 괄호 검사

---
+++ alg/parenthesis.js
@@ -0,0 +1,23 @@
+function check(input) {
+  const stack = [];
+  let temp = 0;
+
+  input = Array.from(input);
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === '(') {
+      stack.push(input[i]);
+      temp += 1;
+    } else if (input[i] === ')') {
+      stack.pop();
+      temp -= 1;
+    }
+
+    if (temp < 0) return false;
+  }
+
+  return stack.length === 0 ? true : false;
+}
+
+const input = '()(())';
+check(input) ? console.log('Correct') : console.log('Error');

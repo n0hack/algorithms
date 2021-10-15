de33552732a4e1ed8e04b3f4db55f645349894b8
Ji-Hun Jeon <nohack-@naver.com>
2021-10-15 21:44:24 +0900

    구구단

---
+++ oj/acmicpc/2739.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input;
+
+rl.on('line', function (line) {
+  input = Number(line);
+}).on('close', function () {
+  for (let i = 1; i <= 9; i++) {
+    console.log(`${input} * ${i} = ${input * i}`);
+  }
+  process.exit();
+});

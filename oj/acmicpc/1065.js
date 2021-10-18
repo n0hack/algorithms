5dfd29ce593526b7a4fe1ec5bceb78318f6eab2b
Ji-Hun Jeon <nohack-@naver.com>
2021-10-18 20:32:49 +0900

    한수

--- oj/acmicpc/1065.js
+++ oj/acmicpc/1065.js
@@ -11,22 +11,25 @@ rl.on('line', function (line) {
 }).on('close', function () {
   let ans = 0;
 
-  ans = 99;
+  if (input < 100) {
+    ans = input;
+  } else {
+    ans = 99;
 
-  for (let i = 100; i <= input; i++) {
-    let strNum = String(i);
-    let temp = Number(strNum[1]) - Number(strNum[0]);
-    let j = 1;
-    for (j = 1; j < strNum.length - 1; j++) {
-      let temp2 = Number(strNum[j + 1]) - Number(strNum[j]);
-      if (temp === temp2) temp = temp2;
-      else break;
-    }
-    if (j === strNum.length - 1) {
-      ans++;
+    for (let i = 100; i <= input; i++) {
+      let strNum = String(i);
+      let temp = Number(strNum[1]) - Number(strNum[0]);
+      let j = 1;
+      for (j = 1; j < strNum.length - 1; j++) {
+        let temp2 = Number(strNum[j + 1]) - Number(strNum[j]);
+        if (temp === temp2) temp = temp2;
+        else break;
+      }
+      if (j === strNum.length - 1) {
+        ans++;
+      }
     }
   }
-
   console.log(ans);
   process.exit();
 });

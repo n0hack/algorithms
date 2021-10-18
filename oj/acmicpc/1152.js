b20e29720aed5b506783e0bd9e2c2f72dc590dcb
Ji-Hun Jeon <nohack-@naver.com>
2021-10-19 06:41:41 +0900

    단어의 개수

---
+++ oj/acmicpc/1152.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input = line.split(' ');
+}).on('close', function () {
+  input = input.filter((item) => item !== '');
+  console.log(input.length);
+  process.exit();
+});

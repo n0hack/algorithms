90b10772f96d8b022b7d87f44169a90b023eadc1
Ji-Hun Jeon <nohack-@naver.com>
2021-10-18 13:17:26 +0900

    BST 추가 구현

--- ds/binary-tree.js
+++ ds/binary-tree.js
@@ -3,7 +3,7 @@ class Node {
     this.data = data;
     this.left = null;
     this.right = null;
-    // 반복문을 이용한 순회에 사용할 변수
+    // 반복문을 이용한 후위 순회에 사용할 변수
     this.visited = false;
   }
 }
@@ -11,6 +11,73 @@ class Node {
 class BinaryTree {
   constructor(node) {
     this.root = node;
+  }
+
+  // Target과 일치하는 노드 탐색
+  searchNode(target, tree = this.root) {
+    if (tree === null) return null;
+
+    if (tree.data === target) return tree;
+    else if (tree.data < target) this.searchNode(target, tree.right);
+    else this.searchNode(target, tree.left);
+  }
+
+  // 트리에서 가장 작은 노드 탐색
+  searchMinNode(tree = this.root) {
+    if (tree === null) return null;
+
+    if (tree.left === null) return tree;
+    else this.searchMinNode(tree.left);
+  }
+
+  // 노드 삽입
+  insertChild(newNode, tree = this.root) {
+    if (tree.data < newNode.data) {
+      if (tree.right === null) tree.right = newNode;
+      else this.insertChild(newNode, tree.right);
+    } else if (tree.data > newNode.data) {
+      if (tree.left === null) tree.left = newNode;
+      else this.insertChild(newNode, tree.left);
+    }
+  }
+
+  // 노드 제거
+  removeChild(target, tree = this.root, parent = null) {
+    let removed = tree;
+
+    if (tree === null) {
+      return null;
+    }
+
+    if (tree.data > target) {
+      removed = this.removeChild(target, tree.left, tree);
+    } else if (tree.data < target) {
+      removed = this.removeChild(target, tree.right, tree);
+    } else {
+      // 잎 노드인 경우 바로 제거
+      if (tree.left === null && tree.right === null) {
+        if (parent.left === tree) parent.left = null;
+        else parent.right = null;
+      } else {
+        // 자식이 양쪽 모두 있는 경우
+        if (tree.left !== null && tree.right !== null) {
+          let minNode = this.getSearchMinNode(tree.right);
+          // 찾은 노드도 제거해야 함
+          minNode = this.removeChild(minNode.data, tree, null);
+          tree.data = minNode.data;
+        } else {
+          // 자식이 하나만 있는 경우
+          let temp;
+
+          if (tree.left !== null) temp = tree.left;
+          else temp = tree.right;
+
+          if (parent.left === tree) parent.left = temp;
+          else parent.right = temp;
+        }
+      }
+    }
+    return removed;
   }
 
   insertLeftChild(parentNode, childNode) {
@@ -152,50 +219,73 @@ class BinaryTree {
     return inner(root);
   }
 }
-const A = new Node('A');
-const B = new Node('B');
-const C = new Node('C');
-const D = new Node('D');
-const E = new Node('E');
-const F = new Node('F');
-const G = new Node('G');
 
-const tree = new BinaryTree(A);
-
-tree.insertLeftChild(A, B);
-tree.insertRightChild(A, C);
-tree.insertLeftChild(B, D);
-tree.insertRightChild(B, E);
-tree.insertLeftChild(C, F);
-tree.insertRightChild(C, G);
-
-console.log('--preorder--');
-tree.preorder();
-console.log();
-
-console.log('--inorder--');
+const tree = new BinaryTree(new Node(123));
+tree.insertChild(new Node(22));
+tree.insertChild(new Node(9918));
+tree.insertChild(new Node(424));
+tree.insertChild(new Node(17));
+tree.insertChild(new Node(3));
+tree.insertChild(new Node(98));
+tree.insertChild(new Node(34));
+tree.insertChild(new Node(760));
+tree.insertChild(new Node(317));
+tree.insertChild(new Node(1));
 tree.inorder();
 console.log();
 
-console.log('--postorder--');
-tree.postorder();
+tree.removeChild(98);
+tree.inorder();
 console.log();
 
-console.log('--preorder (Loop) --');
-tree.preorderByLoop();
+tree.insertChild(new Node(111));
+tree.inorder();
 console.log();
 
-console.log('--inorder (Loop) --');
-tree.inorderByLoop();
-console.log();
+// const A = new Node('A');
+// const B = new Node('B');
+// const C = new Node('C');
+// const D = new Node('D');
+// const E = new Node('E');
+// const F = new Node('F');
+// const G = new Node('G');
 
-console.log('--postorder (Loop) --');
-tree.postorderByLoop();
-console.log();
+// const tree = new BinaryTree(A);
 
-console.log('--levelorder --');
-tree.levelorder();
-console.log();
+// tree.insertLeftChild(A, B);
+// tree.insertRightChild(A, C);
+// tree.insertLeftChild(B, D);
+// tree.insertRightChild(B, E);
+// tree.insertLeftChild(C, F);
+// tree.insertRightChild(C, G);
 
-console.log(tree.getNodeCount());
-console.log(tree.getLeafNodeCount());
+// console.log('--preorder--');
+// tree.preorder();
+// console.log();
+
+// console.log('--inorder--');
+// tree.inorder();
+// console.log();
+
+// console.log('--postorder--');
+// tree.postorder();
+// console.log();
+
+// console.log('--preorder (Loop) --');
+// tree.preorderByLoop();
+// console.log();
+
+// console.log('--inorder (Loop) --');
+// tree.inorderByLoop();
+// console.log();
+
+// console.log('--postorder (Loop) --');
+// tree.postorderByLoop();
+// console.log();
+
+// console.log('--levelorder --');
+// tree.levelorder();
+// console.log();
+
+// console.log(tree.getNodeCount());
+// console.log(tree.getLeafNodeCount());

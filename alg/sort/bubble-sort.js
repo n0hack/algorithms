5e4c06ca337d0d00553d3b85de15b8fb7d141bc1
n0hack <nohack-@naver.com>
2021-10-27 02:35:43 +0900

    Bubble Sort

---
+++ alg/sort/bubble-sort.js
@@ -0,0 +1,16 @@
+// 버블 정렬
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+};
+
+const arr = [1, 5, 4, 2, 3];
+
+console.log(arr);
+bubbleSort(arr);
+console.log(arr);

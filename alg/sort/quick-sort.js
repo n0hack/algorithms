841fde14de35664b2b05bc97002ad8e10e362469
n0hack <nohack-@naver.com>
2021-10-27 07:14:43 +0900

    Quick Sort

---
+++ alg/sort/quick-sort.js
@@ -0,0 +1,32 @@
+// 퀵 정렬
+// 병합 정렬처럼 분할 정복 컨셉의 알고리즘
+
+const partition = (arr, left, right) => {
+  const pivot = left++;
+
+  while (left < right) {
+    while (arr[left] <= arr[pivot]) left++;
+    while (arr[right] > arr[pivot]) right--;
+
+    if (left < right) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+    }
+  }
+  [arr[pivot], arr[right]] = [arr[right], arr[pivot]];
+  console.log(arr, left, right);
+  return right;
+};
+
+const quickSort = (arr, left, right) => {
+  if (left < right) {
+    const index = partition(arr, left, right);
+    quickSort(arr, left, index - 1);
+    quickSort(arr, index + 1, right);
+  }
+};
+
+const arr = [1, 5, 4, 2, 3];
+
+console.log(arr);
+quickSort(arr, 0, arr.length - 1);
+console.log(arr);

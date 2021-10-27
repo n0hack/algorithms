8a980359a63036ca13ed4574065b8c13aeb6de7b
n0hack <nohack-@naver.com>
2021-10-27 23:04:56 +0900

    Bubble Sort

--- alg/sort/bubble-sort.js
+++ alg/sort/bubble-sort.js
@@ -1,5 +1,14 @@
 // 버블 정렬
 const bubbleSort = (arr) => {
+  // 정렬이 필요 없는 경우
+  let needSort = false;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] > arr[i]) {
+      needSort = true;
+    }
+  }
+  if (!needSort) return;
+
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {

e9760dc8bcf90ddb476d8f5735257b2d658fd037
n0hack <nohack-@naver.com>
2021-10-28 00:21:04 +0900

    소수 찾기 (에라토스테네스의 체)

--- algorithms/prime.js
+++ algorithms/prime.js
@@ -1,8 +1,4 @@
-/**
- * 에라스토테네스의 체를 이용해 소수 리스트 생성
- * @param {number} n
- * @returns 소수가 담긴 배열
- */
+// 소수 찾기
 function getPrimeList(n) {
   const ret = [];
   const temp = Array.from({ length: n }, () => 0);

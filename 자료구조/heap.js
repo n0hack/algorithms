71a5fc33d2a43f079513b7a99ccbdf2377b5e3d9
n0hack <nohack-@naver.com>
2021-10-29 23:25:10 +0900

    힙 (Heap: Min + Max)

--- 자료구조/heap.js
+++ 자료구조/heap.js
@@ -9,82 +9,126 @@ class Node {
 }
 
 class Heap extends Array {
-  constructor() {
+  constructor({ type }) {
     super();
     this.usedSize = 0;
+    // type: min or max
+    this.type = type;
+  }
+
+  insert(newData) {
+    let currentPosition = this.usedSize;
+    let parentPosition = this.getParent(currentPosition);
+
+    // 새로운 데이터 추가
+    this.push(newData);
+
+    if (this.type === 'min') {
+      // min-heap
+      while (currentPosition > 0 && this[currentPosition].data < this[parentPosition].data) {
+        this.swap(currentPosition, parentPosition);
+        currentPosition = parentPosition;
+        parentPosition = this.getParent(currentPosition);
+      }
+    } else {
+      // max-heap
+      while (currentPosition > 0 && this[currentPosition].data > this[parentPosition].data) {
+        this.swap(currentPosition, parentPosition);
+        currentPosition = parentPosition;
+        parentPosition = this.getParent(currentPosition);
+      }
+    }
+    this.usedSize += 1;
+  }
+
+  deleteMin() {
+    const deleteMin = this[0];
+    this.usedSize -= 1;
+
+    if (this.usedSize === 0) {
+      // 노드가 하나뿐인 경우, 그대로 제거 후 반환
+      this.pop();
+      return deleteMin;
+    } else {
+      // 마지막 노드를 루트로 가져 옴
+      this[0] = this.pop(this.usedSize);
+    }
+
+    let parentPosition = 0;
+    let leftPosition = this.getLeftChild(parentPosition);
+    let rightPosition = leftPosition + 1;
+
+    // 힙 재구성
+    while (true) {
+      let selectedChild = 0;
+
+      // leftPosition이 usedSize보다 크면 바꿀 필요 없음
+      if (leftPosition >= this.usedSize) break;
+      if (rightPosition >= this.usedSize) {
+        selectedChild = leftPosition;
+
+        if (this.type === 'min') {
+          // min-heap
+          if (this[selectedChild].data < this[parentPosition].data) {
+            this.swap(selectedChild, parentPosition);
+            parentPosition = selectedChild;
+          } else {
+            break;
+          }
+        } else {
+          // max-heap
+          if (this[selectedChild].data > this[parentPosition].data) {
+            this.swap(selectedChild, parentPosition);
+            parentPosition = selectedChild;
+          } else {
+            break;
+          }
+        }
+      } else {
+        if (this[leftPosition].data > this[rightPosition].data) selectedChild = rightPosition;
+        else selectedChild = leftPosition;
+
+        if (this.type === 'min') {
+          // min-heap
+          if (this[selectedChild].data < this[parentPosition].data) {
+            this.swap(selectedChild, parentPosition);
+            parentPosition = selectedChild;
+          } else {
+            break;
+          }
+        } else {
+          // max-heap
+          if (this[selectedChild].data > this[parentPosition].data) {
+            this.swap(selectedChild, parentPosition);
+            parentPosition = selectedChild;
+          } else {
+            break;
+          }
+        }
+      }
+      leftPosition = this.getLeftChild(parentPosition);
+      rightPosition = leftPosition + 1;
+    }
+    return deleteMin;
+  }
+
+  // 유틸 메소드
+  swap(index1, index2) {
+    let temp = this[index1];
+    this[index1] = this[index2];
+    this[index2] = temp;
   }
 
   getParent(index) {
     return Math.floor((index - 1) / 2);
   }
 
-  insert(newData) {
-    let currentPosition = this.usedSize;
-    let parentPosition = this.getParent(currentPosition);
-    this.push(newData);
-
-    while (currentPosition > 0 && this[currentPosition].data < this[parentPosition].data) {
-      // Swap
-      let temp = this[currentPosition];
-      this[currentPosition] = this[parentPosition];
-      this[parentPosition] = temp;
-
-      currentPosition = parentPosition;
-      parentPosition = this.getParent(currentPosition);
-    }
-    this.usedSize += 1;
-  }
-
   getLeftChild(index) {
     return index * 2 + 1;
   }
-
-  deleteMin() {
-    let parentPosition = 0;
-    let leftPosition = 0;
-    let rightPosition = 0;
-
-    const deleteMin = this[0];
-
-    // 마지막 노드를 루트 위치로 이동
-    this.usedSize -= 1;
-    this[0] = this.pop(this.usedSize);
-
-    leftPosition = this.getLeftChild(0);
-    rightPosition = leftPosition + 1;
-
-    // 힙이 제대로 구성될 때까지 반복
-    while (true) {
-      let selectedChild = 0;
-
-      // leftPosition이 usedSize보다 크면 바꿀 필요 없음
-      if (leftPosition >= this.usedSize) break;
-      if (rightPosition >= this.usedSize) selectedChild = leftPosition;
-      else {
-        if (this[leftPosition].data > this[rightPosition].data) selectedChild = rightPosition;
-        else selectedChild = leftPosition;
-        console.log(`${leftPosition} 선택`);
-
-        // 선택한 값이 부모보다 작다면 교환
-        if (this[selectedChild].data < this[parentPosition].data) {
-          let temp = this[selectedChild];
-          this[selectedChild] = this[parentPosition];
-          this[parentPosition] = temp;
-          parentPosition = selectedChild;
-        } else {
-          break;
-        }
-
-        leftPosition = this.getLeftChild(parentPosition);
-        rightPosition = leftPosition + 1;
-      }
-      // console.log(heap, leftPosition, rightPosition);
-    }
-    return deleteMin;
-  }
 }
 
-const heap = new Heap();
+const heap = new Heap({ type: 'max' });
 heap.insert(new Node(2));
 heap.insert(new Node(7));
 heap.insert(new Node(52));
@@ -96,5 +140,5 @@ heap.insert(new Node(17));
 heap.insert(new Node(43));
 heap.insert(new Node(88));
 heap.insert(new Node(37));
+console.log(heap);
 console.log(heap.deleteMin());
-// console.log(heap);

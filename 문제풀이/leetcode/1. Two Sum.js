c3b9dfaa69496830a694b6d385688fe57a22255d
n0hack <nohack-@naver.com>
2021-11-06 13:11:33 +0900

    두 수의 합

--- 문제풀이/leetcode/1. Two Sum.js
+++ 문제풀이/leetcode/1. Two Sum.js
@@ -3,17 +3,26 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    let temp = target - nums[i];
-    let find = nums.findIndex((num, j) => num === temp && i !== j);
-    if (find !== -1) return [i, find];
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     let temp = target - nums[i];
+//     let find = nums.findIndex((num, j) => num === temp && i !== j);
+//     if (find !== -1) return [i, find];
+//   }
+//   // for (let i = 0; i < nums.length - 1; i++) {
+//   //   for (let j = i + 1; j < nums.length; j++) {
+//   //     if (nums[i] + nums[j] === target) return [i, j];
+//   //   }
+//   // }
+// };
+
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length - 1; i++) {
+    const sub = target - nums[i];
+    const numsWithoutSub = nums.slice(i + 1);
+
+    if (numsWithoutSub.includes(sub)) return [i, nums.indexOf(sub, i + 1)];
   }
-  // for (let i = 0; i < nums.length - 1; i++) {
-  //   for (let j = i + 1; j < nums.length; j++) {
-  //     if (nums[i] + nums[j] === target) return [i, j];
-  //   }
-  // }
 };
 
 console.log(twoSum([2, 7, 11, 15], 9));

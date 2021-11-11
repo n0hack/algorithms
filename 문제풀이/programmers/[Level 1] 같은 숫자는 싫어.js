0014af9c0c10db8e15437a9f588d61141147f113
n0hack <nohack-@naver.com>
2021-11-11 23:54:07 +0900

    [Level 1] 같은 숫자는 싫어

---
+++ 문제풀이/programmers/[Level 1] 같은 숫자는 싫어.js
@@ -0,0 +1,17 @@
+const solution = (arr) => {
+  const answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) continue;
+    else answer.push(arr[i]);
+  }
+
+  return answer;
+};
+
+const solution2 = (arr) => {
+  return arr.filter((val, idx) => val !== arr[idx + 1]);
+};
+
+console.log(solution2([1, 1, 3, 3, 0, 1, 1]));
+console.log(solution2([4, 4, 4, 3, 3]));

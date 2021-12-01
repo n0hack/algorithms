14397d3d9e0b23dd3abfbf592e84c124deaa4066
n0hack <nohack-@naver.com>
2021-12-01 21:37:49 +0900

    퀵 정렬 (Quick Sort)

--- 알고리즘/sort/quick-sort.js
+++ 알고리즘/sort/quick-sort.js
@@ -1,28 +1,26 @@
-// 퀵 정렬
+// const partition = (arr, left, right) => {
+//   const pivot = left++;
 
-const partition = (arr, left, right) => {
-  const pivot = left++;
+//   while (left < right) {
+//     while (arr[left] <= arr[pivot]) left++;
+//     while (arr[right] > arr[pivot]) right--;
 
-  while (left < right) {
-    while (arr[left] <= arr[pivot]) left++;
-    while (arr[right] > arr[pivot]) right--;
+//     if (left < right) {
+//       [arr[left], arr[right]] = [arr[right], arr[left]];
+//     }
+//   }
+//   [arr[pivot], arr[right]] = [arr[right], arr[pivot]];
+//   console.log(arr, left, right);
+//   return right;
+// };
 
-    if (left < right) {
-      [arr[left], arr[right]] = [arr[right], arr[left]];
-    }
-  }
-  [arr[pivot], arr[right]] = [arr[right], arr[pivot]];
-  console.log(arr, left, right);
-  return right;
-};
-
-const quickSort = (arr, left, right) => {
-  if (left < right) {
-    const index = partition(arr, left, right);
-    quickSort(arr, left, index - 1);
-    quickSort(arr, index + 1, right);
-  }
-};
+// const quickSort = (arr, left, right) => {
+//   if (left < right) {
+//     const index = partition(arr, left, right);
+//     quickSort(arr, left, index - 1);
+//     quickSort(arr, index + 1, right);
+//   }
+// };
 
 const arr = [1, 5, 4, 2, 3];
 

9f60ac10c6f45aa98d9244ede2cb42f60edf92cd
n0hack <nohack-@naver.com>
2021-12-01 21:37:36 +0900

    버블 정렬 (Bubble Sort)

--- 알고리즘/sort/bubble-sort.js
+++ 알고리즘/sort/bubble-sort.js
@@ -1,18 +1,15 @@
-// 버블 정렬
 const bubbleSort = (arr) => {
-  // 정렬이 필요 없는 경우
   let needSort = false;
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i - 1] > arr[i]) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] < arr[i]) {
       needSort = true;
+      break;
     }
   }
-
-  if (!needSort) return;
+  if (needSort === false) return;
 
   for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - 1 - i; j++) {
+    for (let j = i; j < arr.length; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }

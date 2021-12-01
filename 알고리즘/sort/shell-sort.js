be0ae4565bc9600d0820ca6fc9a7a307467294c5
n0hack <nohack-@naver.com>
2021-12-01 21:37:30 +0900

    셸 정렬 (Shell Sort)

--- 알고리즘/sort/shell-sort.js
+++ 알고리즘/sort/shell-sort.js
@@ -1,19 +1,15 @@
-// 셸 정렬
-// 정렬할 요소를 그룹으로 나누어 삽입 정렬하고, 병합하는 식으로 동작
-
 const shellSort = (arr) => {
-  let len = arr.length;
+  const len = arr.length;
 
   for (let gap = Math.floor(len / 2); gap > 0; gap = Math.floor(gap / 2)) {
-    for (let i = gap; i < len; i += 1) {
-      let temp = arr[i];
+    for (let i = gap; i < len; i++) {
+      const temp = arr[i];
       let j;
 
       for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
         arr[j] = arr[j - gap];
       }
       arr[j] = temp;
-      console.log(arr);
     }
   }
 };

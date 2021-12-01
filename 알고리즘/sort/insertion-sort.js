8e2eb52545f36339b024f35a6ea9e190af577619
n0hack <nohack-@naver.com>
2021-12-01 21:37:43 +0900

    삽입 정렬 (Insertion Sort)

--- 알고리즘/sort/insertion-sort.js
+++ 알고리즘/sort/insertion-sort.js
@@ -1,11 +1,8 @@
-// 삽입 정렬
-// 정렬이 필요한 요소를 뽑아내어, 이를 적당한 곳에 삽입해 나가는 정렬
-
 const insertionSort = (arr) => {
   for (let i = 1; i < arr.length; i++) {
     if (arr[i - 1] <= arr[i]) continue;
 
-    for (let j = i; j > 0; j--) {
+    for (j = i; j > 0; j--) {
       if (arr[j - 1] > arr[j]) {
         [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
       }

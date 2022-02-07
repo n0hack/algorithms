84fa99d2e521b233bda0d7dee56182372b1ce7b2
n0hack <nohack-@naver.com>
2022-02-07 20:53:46 +0900

    T 기업 코딩 테스트

---
+++ 문제풀이/연습문제/정렬/위에서 아래로.js
@@ -0,0 +1,18 @@
+// T 기업 코딩 테스트
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', (line) => {
+  input.push(line);
+}).on('close', () => {
+  const numbers = input.map((item) => parseInt(item)).slice(1);
+
+  numbers.sort((a, b) => a - b).reverse();
+  console.log(numbers);
+  process.exit();
+});

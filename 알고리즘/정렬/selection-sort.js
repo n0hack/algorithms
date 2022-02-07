9aaeb2505e9265cebfe75b3baf949203994465e3
n0hack <nohack-@naver.com>
2022-02-07 14:33:05 +0900

    선택 정렬 (Selection Sort)

---
+++ 알고리즘/정렬/selection-sort.js
@@ -0,0 +1,19 @@
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) minIndex = j;
+    }
+
+    if (minIndex !== i) {
+      [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+    }
+  }
+};
+
+const arr = [1, 3, 5, 2, 4];
+
+console.log(arr);
+selectionSort(arr);
+console.log(arr);

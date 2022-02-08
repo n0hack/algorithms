db68de81950e31c384f791233bd55e70a1182ce4
n0hack <nohack-@naver.com>
2022-02-08 22:13:47 +0900

    퀵 정렬 (Quick Sort Iterative)

--- 알고리즘/정렬/quick-sort-4.js
+++ 알고리즘/정렬/quick-sort-4.js
@@ -1,20 +1,30 @@
 // Iterative Quick Sort
-const quickSort = (arr, start, end) => {
+function partition(arr, start, end) {
+  const pivot = arr[start];
+  let [left, right] = [start + 1, end];
+
+  while (left <= right) {
+    while (left <= right && arr[left] <= pivot) left++;
+    while (left <= right && arr[right] > pivot) right--;
+
+    if (left <= right) [arr[left], arr[right]] = [arr[right], arr[left]];
+    else [arr[start], arr[right]] = [arr[right], arr[start]];
+  }
+  return right;
+}
+
+function quickSort(arr, start, end) {
   const stack = [];
   stack.push([start, end]);
 
   while (stack.length !== 0) {
-    let [left, right] = stack.pop();
-    const pivot = left++;
+    const [start, end] = stack.pop();
+    const pivot = partition(arr, start, end);
 
-    while (left <= right) {
-      while (arr[left] <= arr[pivot]) left++;
-      while (arr[right] > arr[pivot]) right--;
-
-      if (left <= right) [arr[left], arr[pivot]] = [arr[pivot], arr[left]];
-    }
+    if (pivot - 1 > start) stack.push([start, pivot - 1]);
+    if (pivot + 1 < end) stack.push([pivot + 1, end]);
   }
-};
+}
 
 const arr = [5, 4, 2, 6, 1, 3, 7];
 quickSort(arr, 0, arr.length - 1);

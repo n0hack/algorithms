6bc3b0dd8e2d315f2cc2493256fae3e06792dc61
n0hack <nohack-@naver.com>
2022-02-09 23:33:17 +0900

    [기출 문제] 이진 탐색

--- 문제풀이/연습문제/탐색/부품 찾기.js
+++ 문제풀이/연습문제/탐색/부품 찾기.js
@@ -5,15 +5,15 @@ const rl = readline.createInterface({
 });
 
 const binarySearch = (arr, target) => {
-  let left = 0;
-  let right = arr.length - 1;
+  let start = 0;
+  let end = arr.length - 1;
 
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
 
     if (arr[mid] === target) return 'yes';
-    else if (arr[mid] > target) right = mid - 1;
-    else if (arr[mid] < target) left = mid + 1;
+    else if (arr[mid] > target) end = mid - 1;
+    else if (arr[mid] < target) start = mid + 1;
   }
   return 'no';
 };

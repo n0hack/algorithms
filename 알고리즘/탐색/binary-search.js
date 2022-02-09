8ef75e746956c61ce1b45bf620db1e95f0999b7a
n0hack <nohack-@naver.com>
2022-02-09 20:49:43 +0900

    이진 탐색 (Binary Search)

--- 알고리즘/탐색/binary-search.js
+++ 알고리즘/탐색/binary-search.js
@@ -1,13 +1,13 @@
-const search = (arr, left, right, n) => {
+const search = (arr, left, right, target) => {
   if (left >= right) {
     return -1;
   }
 
   const mid = Math.floor((left + right) / 2);
 
-  if (arr[mid] === n) return mid;
-  else if (arr[mid] < n) return search(arr, mid + 1, right, n);
-  else if (arr[mid] > n) return search(arr, left, mid - 1, n);
+  if (arr[mid] === target) return mid;
+  else if (arr[mid] < target) return search(arr, mid + 1, right, n);
+  else if (arr[mid] > target) return search(arr, left, mid - 1, n);
 };
 
 const arr = [1, 7, 11, 12, 14, 23, 67, 139, 871];

5275f5de975b3ad91c31ee4cbd3810751f9b1f75
n0hack <nohack-@naver.com>
2022-02-10 19:00:50 +0900

    팩토리얼 (Factorial)

--- 알고리즘/재귀/factorial.js
+++ 알고리즘/재귀/factorial.js
@@ -11,4 +11,11 @@ const factorial2 = (n) => {
   return res;
 };
 
-console.log(factorial(5));
+// 꼬리 재귀 (Tail Recursion)
+// 재귀 호출이 끝나면, 아무 연산 없이 값만 반환하는 형태
+const factorial3 = (n, total = 1) => {
+  if (n === 1) return total;
+  else return factorial3(n - 1, total * n);
+};
+
+console.log(factorial3(5));

3e8fade64c0d74767d34fe8aa4014bb57c8efee5
n0hack <nohack-@naver.com>
2022-02-16 17:17:17 +0900

    [재귀] 파스칼의 삼각형 출력하기 1

--- 문제풀이/CodeUp/1858.js
+++ 문제풀이/CodeUp/1858.js
@@ -1,31 +1,7 @@
-const pascal = [];
-
-const makePascal = (row, col, max) => {
-  if (col > max) return;
-
-  if (col === 0) pascal.push([]);
-  if (max === 0) pascal[0].push(1);
-  else {
-    let first, second;
-
-    if (col - 1 < 0) first = 0;
-    else first = pascal[row - 1][col - 1];
-    second = pascal[row - 1][col] ?? 0;
-
-    pascal[row].push(first + second);
-    makePascal(row, col + 1, max);
-  }
+const solution = (a, b) => {
+  if (a === 1 || a === b || b === 1) return 1;
+  return solution(a - 1, b - 1) + solution(a - 1, b);
 };
 
-const solution = (current, row, col) => {
-  if (current >= row) {
-    return pascal[row - 1][col - 1];
-  } else {
-    makePascal(current, 0, current);
-    return solution(current + 1, row, col);
-  }
-};
-
-let [n, k] = [5, 2];
-console.log(solution(0, n, k));
-console.log(pascal);
+let [n, k] = [5, 3];
+console.log(solution(n, k));

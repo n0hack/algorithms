62b65aa66f3e222d46dd6fad51ee27848b5a77d2
n0hack <nohack-@naver.com>
2022-02-19 00:23:54 +0900

    깊이 우선 탐색 (Depth First Search)

--- 알고리즘/그래프/dfs.js
+++ 알고리즘/그래프/dfs.js
@@ -1,16 +1,38 @@
-const graph = require('../../자료구조/그래프/list-graph');
+// Recursive
+const dfs = (graph, v, visited) => {
+  visited[v] = true;
+  process.stdout.write(`${v} `);
 
-const dfs = (v) => {
-  process.stdout.write(`${v.data} `);
-  v.visited = true;
-
-  let e = v.adjacency;
-  while (e !== null) {
-    if (e.to !== null && e.to.visited === false) {
-      dfs(e.to);
-    }
-    e = e.next;
+  for (const i of graph[v]) {
+    if (!visited[i]) dfs(graph, i, visited);
   }
 };
 
-dfs(graph.vertices);
+// Iterative
+const dfs2 = (graph, v, visited) => {
+  const stack = [];
+
+  stack.push(v);
+
+  while (stack.length !== 0) {
+    const popped = stack.pop();
+    if (visited[popped]) continue;
+
+    visited[popped] = true;
+    process.stdout.write(`${popped} `);
+
+    for (let i = graph[popped].length - 1; i >= 0; i--) {
+      if (!visited[graph[popped][i]]) {
+        stack.push(graph[popped][i]);
+      }
+    }
+  }
+};
+
+const graph = [[], [2, 3, 8], [1, 7], [1, 4, 5], [3, 5], [3, 4], [7], [2, 6, 8], [1, 7]];
+
+// prettier-ignore
+dfs(graph, 1, Array.from({ length: graph.length }, () => false));
+console.log();
+// prettier-ignore
+dfs2(graph, 1, Array.from({ length: graph.length }, () => false));

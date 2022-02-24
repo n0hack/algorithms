c66eaf85b7205481eae65af9d82e56364d916769
n0hack <nohack-@naver.com>
2022-02-24 16:14:12 +0900

    [연습 문제] 2중 반복문

--- 문제풀이/프로그래머스/Level 1. 직사각형 별찍기.js
+++ 문제풀이/프로그래머스/Level 1. 직사각형 별찍기.js
@@ -3,7 +3,7 @@ process.stdin.on('data', (data) => {
   const [n, m] = data.split(' ').map((item) => parseInt(item));
 
   for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) process.stdin.write('*');
+    for (let j = 0; j < n; j++) process.stdout.write('*');
     console.log();
   }
 });

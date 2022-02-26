98215434b9b40fef79e2e232c78518dc4d31a53a
n0hack <nohack-@naver.com>
2022-02-27 04:34:27 +0900

    [연습 문제] for, push

---
+++ 문제풀이/프로그래머스/Level 1. 같은 숫자는 싫어.js
@@ -0,0 +1,15 @@
+const solution = (arr) => {
+  const answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i + 1]) answer.push(arr[i]);
+  }
+
+  return answer;
+};
+
+const inputs = [
+  [1, 1, 3, 3, 0, 1, 1],
+  [4, 4, 4, 3, 3],
+];
+inputs.forEach((input) => console.log(solution(input)));

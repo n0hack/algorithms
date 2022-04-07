ff467dd9e988594508a99f17ba166225a2c6b507
n0hack <nohack-@naver.com>
2022-04-07 19:46:12 +0900

    [feat] SelectedLanguage 컴포넌트

---
+++ 코딩테스트/2022 Dev-Matching: 웹 프론트엔드 개발자(상반기)/asset/js/components/SelectedLanguage.js
@@ -0,0 +1,33 @@
+const MAX_DISPLAY_COUNT = 5;
+
+export default function SelectedLanguage({ $target, initialState }) {
+  this.$element = document.createElement('div');
+  this.$element.className = 'SelectedLanguage';
+  this.state = initialState;
+
+  $target.append(this.$element);
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+
+    if (this.state.length > MAX_DISPLAY_COUNT) {
+      const startPosition = this.state.length - MAX_DISPLAY_COUNT;
+      this.state = this.state.slice(startPosition, MAX_DISPLAY_COUNT + startPosition);
+    }
+    this.render();
+  };
+
+  this.render = () => {
+    // prettier-ignore
+    this.$element.innerHTML = `
+      <ul>
+        ${this.state.map((item) => `
+          <li>${item}</li>
+        `
+        ).join('')}
+      </ul>
+    `
+  };
+
+  this.render();
+}

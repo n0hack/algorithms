1dad389dcf803c9af967f89b4bb2927da956ca38
Lucid <nohack-@naver.com>
2023-09-03 22:30:39 +0900

    스택 (Stack)

--- 자료구조/스택/stack.js
+++ 자료구조/스택/stack.js
@@ -1,38 +1,44 @@
 function Stack(size) {
-  this.stack = [];
+  this.list = [];
   this.size = size;
   this.top = -1;
 }
 
 Stack.prototype.push = function (data) {
-  if (this.isFull()) throw new Error('Stack is full!');
-
-  this.stack[++this.top] = data;
-};
-
-Stack.prototype.isFull = function () {
-  return this.size - 1 === this.top ? true : false;
+  if (this.isFull()) {
+    throw new Error('Stack is full!');
+  }
+  this.list.push(data);
+  this.top++;
 };
 
 Stack.prototype.pop = function () {
-  if (this.isEmpty()) throw new Error('Stack is empty!');
+  if (this.isEmpty()) {
+    throw new Error('Stack is empty!');
+  }
+  return this.list.pop(this.top--);
+};
 
-  return this.stack[this.top--];
+Stack.prototype.clear = function () {
+  while (!this.isEmpty()) {
+    this.pop();
+  }
+};
+
+Stack.prototype.isFull = function () {
+  return this.top === this.size - 1 ? true : false;
 };
 
 Stack.prototype.isEmpty = function () {
   return this.top === -1 ? true : false;
 };
 
-Stack.prototype.clear = function () {
-  while (this.top !== -1) {
-    this.pop();
-  }
-};
-
 const stack = new Stack(3);
-
 stack.push(1);
 stack.push(2);
 stack.push(3);
-stack.pop();
+console.log(stack.list);
+console.log('popped:', stack.pop());
+console.log(stack.list);
+stack.clear();
+console.log(stack.list);

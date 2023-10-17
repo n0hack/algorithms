46f2bb91e6f907a50ec6aaa8d596894b31a150ff
Lucid <nohack-@naver.com>
2023-10-17 15:28:44 +0900

    Easy - Runtime (56ms, 87.15%) / Memory (44.79MB, 58.74%)

---
+++ 문제풀이/LeetCode/00001. Two Sum.ts
@@ -0,0 +1,20 @@
+// Runtime (56ms, 87.15%) / Memory (44.79MB, 58.74%)
+import { test } from './utils/test';
+
+function twoSum(nums: number[], target: number): number[] {
+  const map = nums.reduce((obj, num, i) => {
+    obj[num] = i;
+    return obj;
+  }, {} as Record<number, number>);
+
+  for (let i = 0; i < nums.length; i++) {
+    const sub = target - nums[i];
+    if (sub in map && map[sub] !== i) return [i, map[sub]];
+  }
+
+  return [];
+}
+
+test(() => twoSum([2, 7, 11, 15], 9));
+test(() => twoSum([3, 2, 4], 6));
+test(() => twoSum([3, 3], 6));

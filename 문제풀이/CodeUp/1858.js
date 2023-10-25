5e13f94f46b904fa00b0d0d09044ae564d4d7b6f
Lucid <nohack-@naver.com>
2023-10-25 14:33:55 +0900

    코드업 삭제

--- 문제풀이/CodeUp/1858.js
+++
@@ -1,7 +0,0 @@
-const solution = (a, b) => {
-  if (a === 1 || a === b || b === 1) return 1;
-  return solution(a - 1, b - 1) + solution(a - 1, b);
-};
-
-let [n, k] = [5, 3];
-console.log(solution(n, k));
--- 문제풀이/CodeUp/1920.js
+++
@@ -1,9 +0,0 @@
-const toBinary = (n) => {
-  if (n <= 1) {
-    process.stdout.write(`${n}`);
-  } else {
-    toBinary(Math.floor(n / 2));
-    process.stdout.write(`${n % 2}`);
-  }
-};
-toBinary(11);

027823b2edc14aa8a6dd5d927443433ecc3b424a
Lucid <nohack-@naver.com>
2024-04-17 00:18:54 +0900

    Easy - Awaited

--- 문제풀이/TypeChallenges/00189. Awaited.ts
+++ 문제풀이/TypeChallenges/00189. Awaited.ts
@@ -1,27 +1,4 @@
-/*
-  189 - Awaited
-  -------
-  by Maciej Sikora (@maciejsikora) #쉬움 #promise #built-in
-
-  ### 질문
-
-  Promise와 같은 타입에 감싸인 타입이 있을 때, 안에 감싸인 타입이 무엇인지 어떻게 알 수 있을까요?
-
-  예시: 들어 `Promise<ExampleType>`이 있을 때, `ExampleType`을 어떻게 얻을 수 있을까요?
-
-  ```ts
-  type ExampleType = Promise<string>
-
-  type Result = MyAwaited<ExampleType> // string
-  ```
-
-  > 출처: [original article](https://dev.to/macsikora/advanced-typescript-exercises-question-1-45k4) by [@maciejsikora](https://github.com/maciejsikora)
-
-  > GitHub에서 보기: https://tsch.js.org/189/ko
-*/
-
 /* _____________ 여기에 코드 입력 _____________ */
-
 type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U>
   ? U extends PromiseLike<any>
     ? MyAwaited<U>
@@ -47,10 +24,3 @@ type cases = [
 
 // @ts-expect-error
 type error = MyAwaited<number>;
-
-/* _____________ 다음 단계 _____________ */
-/*
-  > 정답 공유하기: https://tsch.js.org/189/answer/ko
-  > 정답 보기: https://tsch.js.org/189/solutions
-  > 다른 문제들: https://tsch.js.org/ko
-*/

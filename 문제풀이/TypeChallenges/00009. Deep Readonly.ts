5073c606c75280d08547d7e2ad5880e1701981d8
Lucid <nohack-@naver.com>
2024-04-19 19:42:31 +0900

    Medium - Deep Readonly

---
+++ 문제풀이/TypeChallenges/00009. Deep Readonly.ts
@@ -0,0 +1,63 @@
+/* _____________ 여기에 코드 입력 _____________ */
+type PassType = string | number | Function;
+
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: T[K] extends PassType ? T[K] : DeepReadonly<T[K]>;
+};
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type T = DeepReadonly<X1>;
+
+type cases = [Expect<Equal<DeepReadonly<X1>, Expected1>>, Expect<Equal<DeepReadonly<X2>, Expected2>>];
+
+type X1 = {
+  a: () => 22;
+  b: string;
+  c: {
+    d: boolean;
+    e: {
+      g: {
+        h: {
+          i: true;
+          j: 'string';
+        };
+        k: 'hello';
+      };
+      l: [
+        'hi',
+        {
+          m: ['hey'];
+        }
+      ];
+    };
+  };
+};
+
+type X2 = { a: string } | { b: number };
+
+type Expected1 = {
+  readonly a: () => 22;
+  readonly b: string;
+  readonly c: {
+    readonly d: boolean;
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true;
+          readonly j: 'string';
+        };
+        readonly k: 'hello';
+      };
+      readonly l: readonly [
+        'hi',
+        {
+          readonly m: readonly ['hey'];
+        }
+      ];
+    };
+  };
+};
+
+type Expected2 = { readonly a: string } | { readonly b: number };

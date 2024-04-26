4b41830978ac840cf8f07855302fac416b47e8d6
Lucid <nohack-@naver.com>
2024-04-26 22:09:11 +0900

    Medium - IsNever

---
+++ 문제풀이/TypeChallenges/01042. IsNever.ts
@@ -0,0 +1,17 @@
+/* _____________ 여기에 코드 입력 _____________ */
+type IsNever<T> = [T] extends [never] ? true : false;
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type T = IsNever<never>;
+
+type cases = [
+  Expect<Equal<IsNever<never>, true>>,
+  Expect<Equal<IsNever<never | string>, false>>,
+  Expect<Equal<IsNever<''>, false>>,
+  Expect<Equal<IsNever<undefined>, false>>,
+  Expect<Equal<IsNever<null>, false>>,
+  Expect<Equal<IsNever<[]>, false>>,
+  Expect<Equal<IsNever<{}>, false>>
+];

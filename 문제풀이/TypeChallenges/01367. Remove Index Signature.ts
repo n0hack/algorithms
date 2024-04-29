2e22f7a0e107a6e49b499721151e4656f50b25b8
Lucid <nohack-@naver.com>
2024-04-29 15:08:08 +0900

    Medium - Remove Index Signature

---
+++ 문제풀이/TypeChallenges/01367. Remove Index Signature.ts
@@ -0,0 +1,42 @@
+/* _____________ 여기에 코드 입력 _____________ */
+type IndexSignature = {
+  [key: string | number | symbol]: any;
+};
+
+type RemoveIndexSignature<T, P = PropertyKey> = {
+  [K in keyof T as P extends K ? never : K extends P ? K : never]: T[K];
+};
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Foo = {
+  [key: string]: any;
+  foo(): void;
+};
+
+type Bar = {
+  [key: number]: any;
+  bar(): void;
+  0: string;
+};
+
+const foobar = Symbol('foobar');
+type FooBar = {
+  [key: symbol]: any;
+  [foobar](): void;
+};
+
+type Baz = {
+  bar(): void;
+  baz: string;
+};
+
+type T = RemoveIndexSignature<Foo>;
+
+type cases = [
+  Expect<Equal<RemoveIndexSignature<Foo>, { foo(): void }>>,
+  Expect<Equal<RemoveIndexSignature<Bar>, { bar(): void; 0: string }>>,
+  Expect<Equal<RemoveIndexSignature<FooBar>, { [foobar](): void }>>,
+  Expect<Equal<RemoveIndexSignature<Baz>, { bar(): void; baz: string }>>
+];
